91b29c50a4aa58cdea247747c5ea5718f9f94f81
Yizen <ogrenet@gmail.com>
2011-06-22 17:06:19 +0200

    Selection rotation using the rotation dial

--- js/tracks/RotationDial.js
+++ js/tracks/RotationDial.js
@@ -13,10 +13,6 @@
 		this.selected = false;
 		this.visible = false;
 		this.size  = 200;
-		
-		//this.regX = -this.size / 2;
-		//this.regY = -this.size / 2;
-		
 		this.selection = null;
 		
 		this.makeShape();
@@ -35,20 +31,43 @@
 	
 	RotationDial.prototype.hide = function () {
 		this.visible = false;
+		this.rotation = 0;
 		setDirty();
 	
 	}
 	
 	RotationDial.prototype.onPress = function (evt) {
 	
+		var origX = evt.stageX;
+        var origY = evt.stageY;
+        var origRotation = 0;
+        
+        var dial = this;
+        var prevAngle = 0;
+       
 		evt.onMouseMove = function (evt) {
-		
-		
+            var angle = dial.getAngle(new Point2D(origX, origY),new Point2D(evt.stageX,evt.stageY));
+            
+            //FIXME : something must be wrong somewhere, we shouldn't have to substract 180.
+            angle = angle - 180;
+            delta = angle - prevAngle;
+
+            dial.rotation = angle;
+            
+            for (var i=0; i<dial.selection.length; i++) {
+            	var refPoint = new Point2D(dial.selection[i].x, dial.selection[i].y);
+            	refPoint.rotate(delta, new Point2D(dial.x, dial.y));
+            	
+            	dial.selection[i].move(refPoint.x, refPoint.y);
+            	dial.selection[i].rotate(dial.selection[i].rotation + delta);
+       		}
+       		
+    		prevAngle = angle;
+
 		};
 		
 		evt.onMouseUp = function (evt) {
-		
-		
+					
 		};
 	}
 	
@@ -67,11 +86,25 @@
 		this.graphics.beginFill("rgba(0,0,0,0.2)").drawCircle(0,0,this.size * 0.3).endFill();
 		
 		this.createTicks(10, 0.8, 1, 2);
-		this.createTicks(5,  0.8, 0.9);
-		//this.createTicks(2,  1, 0.95);
-		
+		this.createTicks(5,  0.8, 0.9);		
 		this.createTicks(10, 0.3, 0.4);
-
+		
+		this.graphics.beginStroke("#000000");
+		this.graphics.setStrokeStyle(3);
+		this.graphics.moveTo(0, -this.size/2 * 0.1)
+					 .lineTo(0, +this.size/2 * 0.1)
+					 .moveTo(-this.size/2 * 0.1, 0)
+					 .lineTo(+this.size/2 * 0.1, 0)
+					 .closePath();
+		
+		/*
+		 * Vertical line
+		 *
+		 
+		this.graphics.moveTo(0,0)
+					 .lineTo(0, -this.size/2)
+					 .closePath();
+		 */
 			
 		setDirty();
 	}
@@ -87,7 +120,7 @@
 		var pivot = new Point2D(0,0);
 		
 		var stops = 360 / increment; 
-		
+					
 		for (var i=0;i<stops;i++) {
 			this.graphics.beginStroke(color);
 			this.graphics.setStrokeStyle(stroke);
@@ -104,6 +137,42 @@
 	RotationDial.prototype.getDialTickColor = function() {
 		return colors.defaultDialTickStoke;
 	}
+	
+	RotationDial.prototype.getAngle = function (origin, target) {
+	
+		//Create Vectors
+		var vectorA = new Object;
+		vectorA.p1 = new Point2D(this.x, this.y);
+		vectorA.p2 = new Point2D(origin.x, origin.y);
+		
+		var vectorB = new Object;
+		vectorB.p1 = new Point2D(this.x, this.y);
+		vectorB.p2 = new Point2D(target.x, target.y);
+	
+        // Make reference line a vector
+        var Ax = vectorA.p2.x - vectorA.p1.x;
+        var Ay = vectorA.p2.y - vectorA.p1.y;
+
+        var Bx = vectorB.p1.x - vectorB.p2.x;
+        var By = vectorB.p1.y - vectorB.p2.y;
+
+        // Get the vector length
+        var Alen = Math.sqrt(Ax * Ax + Ay * Ay);
+        var Blen = Math.sqrt(Bx * Bx + By * By);
+
+        // Make unit length
+        // To work the coordinate system with an origin in the lower left rather than upper left corner
+        // negate the y coords by adding a unary minus to rdy and dy in the division below.
+        Ax = Ax / Alen;
+        Ay = Ay / Alen;
+        Bx = Bx / Blen;
+        By = By / Blen;
+
+        // Dot product and convert to degrees
+        // To leave in radians just do: return ( Math.acos ( rdx * dx + rdy * dy ) );
+        //return ( Math.acos ( rdx * dx + rdy * dy ) / 6.28 * 360 );	
+        return (Math.atan2(By, Bx) - Math.atan2(Ay, Ax)) / (Math.PI * 2 / 360);
+    }
 
 	window.RotationDial = RotationDial;
 }(window));
